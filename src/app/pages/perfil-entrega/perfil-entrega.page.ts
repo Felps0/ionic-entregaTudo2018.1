27e9b67c0afc99d02dbb154e3f000e739dd96543
Técnico em Informática <24121742018.1@senacrjedu.local>
2019-10-18 21:42:09 -0300

    18/10/2019

--- src/app/pages/perfil-entrega/perfil-entrega.page.ts
+++ src/app/pages/perfil-entrega/perfil-entrega.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Entrega } from '../../model/entrega';
+import { ActivatedRoute } from '@angular/router';
+import { EntregaService } from 'ionic-entregaTudo2018.1/src/app/services/entrega.service';
 @Component({
   selector: 'app-perfil-entrega',
   templateUrl: './perfil-entrega.page.html',
   styleUrls: ['./perfil-entrega.page.scss'],
 })
 export class PerfilEntregaPage implements OnInit {
+  
+  protected entrega: Entrega = new Entrega;
+  protected id: string = null;
 
-  constructor() { }
+  constructor(
+    protected activatedRoute: ActivatedRoute,
+    protected entregaService: EntregaService,
+  ) { }
 
   ngOnInit() {
+    this.id = this.activatedRoute.snapshot.paramMap.get("id");
+    if (this.id) {
+      this.entregaService.get(this.id).subscribe(
+        res => {
+          this.entrega = res
+        },
+        erro => this.id = null
+      )
+    }
   }
 
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { EntregaService } from '../services/entrega.service';
 
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
 
+export class Tab1Page {
   protected entregas: any;
 
   constructor(
@@ -18,7 +19,6 @@ export class Tab1Page {
         this.entregas = res
       }
     )
-
+    
   }
-
 }
